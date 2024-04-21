0d0befd87e99c64e8725b6083918378292704f3e
liyu-qi <119098640+liyu-qi@users.noreply.github.com>
2024-04-21 14:49:46 +0800

    Delete upload.php

--- upload.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (_FILES['file']['error'] === UPLOAD_ERR_OK) {file = _FILES['file']['tmp_name'];destination = '/path/to/ip a/' . _FILES['file']['name'];
-  move_uploaded_file(file, $destination);
-  echo '文件上传成功！';
-} else {
-  echo '文件上传失败。';
-}
-?>
