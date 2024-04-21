9669a5ffdb100b96e5fb499555baab435f8781ec
liyu-qi <119098640+liyu-qi@users.noreply.github.com>
2024-04-21 14:37:04 +0800

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,6 +1,6 @@
 <?php
 if (_FILES['file']['error'] === UPLOAD_ERR_OK) {file = _FILES['file']['tmp_name'];destination = '/path/to/destination/' . _FILES['file']['name'];
-  move_uploaded_file(file, $destination);
+  move_uploaded_file(file, $ipa);
   echo '文件上传成功！';
 } else {
   echo '文件上传失败。';
